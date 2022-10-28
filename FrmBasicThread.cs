57197186418a1c7c883fe5060bccf60826064fbb
Ashley Dela Cruz <116328595+ashdlacrz@users.noreply.github.com>
2022-10-28 13:15:54 +0800

    Add files via upload

---
+++ FrmBasicThread.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04LabExercise1
+{
+    public partial class FrmBasicThread : Form
+    {
+        private Thread ThreadA, ThreadB;
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart delThread = new ThreadStart(MyThreadClass.Thread1);
+            Console.WriteLine("-Before Starting the Thread-");
+
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+            ThreadA.Name = "Thread A";
+
+            Thread ThreadB = new Thread(MyThreadClass.Thread1);
+            ThreadB.Name = "Thread B";
+
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadA.Join();
+            ThreadB.Join();
+
+            Console.WriteLine("-End of Thread-");
+            label1.Text = "     -End of Thread-";
+        }
+
+        private void FrmBasicThread_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _04LabExercise1
+{
+    class MyThreadClass
+    {
+
+        public static void Thread1()
+        {
+            for(int a = 0; a < 6; a++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + a);
+                Thread.Sleep(1500); //Thread sleep or pause for 1.5 seconds.
+            }
+
+        }
+
+    }
+}
